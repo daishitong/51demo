692eef372be89df4132d7328b967e50472d0a720
daishitong <daishitong@xaut.edu.cn>
2017-05-04 18:44:45 +0800

    modify StepperMotor code.

--- 9.StepperMotor_Control/main.c
+++ 9.StepperMotor_Control/main.c
@@ -5,18 +5,21 @@
 
 void main()
 {
-	unsigned char i;
+    unsigned char i;
 
-    for(i = 0;i < 128;i++)
+    while(1)
     {
-        StepperMotor_Run_CW();
-    }
-    for(i = 0;i < 128;i++)
-    {
-        StepperMotor_Run_CCW();
-    }
-    for(i = 0;i < 128;i++)
-    {
-        delay_nms(10);
+        for(i = 0;i < 128;i++)
+        {
+            StepperMotor_Run_CW();
+        }
+        for(i = 0;i < 128;i++)
+        {
+            StepperMotor_Run_CCW();
+        }
+        for(i = 0;i < 128;i++)
+        {
+            delay_nms(10);
+        }
     }
 }
