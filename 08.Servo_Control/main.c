978bb133904633213444fdfa9502006b093e8691
dst <daishitong@xaut.edu.cn>
2018-06-10 10:51:30 +0800

    modify Servo_Control

--- 08.Servo_Control/main.c
+++ 08.Servo_Control/main.c
@@ -8,22 +8,43 @@ void main()
     // 舵机PWM波时钟初始化
     Timer2_PWM_Init();
     
+    // 将舵机归零,启动PWM波
+    Timer2_PWM_Set(1500,20000);
+    Timer2_PWM_Start();
+    delay_nms(3000);
+    
     while (1)
-    {
-        // 舵机的脉宽为1500us，低电平为20ms，保持2秒
-        Timer2_PWM_Set(1500,20000);
+    {       
+        // 舵机的脉宽为1750us，低电平为20ms，保持2秒
+        Timer2_PWM_Set(1750,20000);
         delay_nms(2000);
         
         // 舵机的脉宽为2000us，低电平为20ms，保持2秒
         Timer2_PWM_Set(2000,20000);
         delay_nms(2000);
+
+        // 舵机的脉宽为1750us，低电平为20ms，保持2秒
+        Timer2_PWM_Set(1750,20000);
+        delay_nms(2000);
         
         // 舵机的脉宽为1500us，低电平为20ms，保持2秒
         Timer2_PWM_Set(1500,20000);
         delay_nms(2000);
-
+        
+        // 舵机的脉宽为1250us，低电平为20ms，保持2秒
+        Timer2_PWM_Set(1250,20000);
+        delay_nms(2000);
+        
         // 舵机的脉宽为1000us，低电平为20ms，保持2秒
         Timer2_PWM_Set(1000,20000);
+        delay_nms(2000);
+                
+        // 舵机的脉宽为1250us，低电平为20ms，保持2秒
+        Timer2_PWM_Set(1250,20000);
+        delay_nms(2000);
+        
+        // 舵机的脉宽为1500us，低电平为20ms，保持3秒
+        Timer2_PWM_Set(1500,20000);
         delay_nms(2000);
     }
 }
